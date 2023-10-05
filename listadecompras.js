84b98fd25157e324132d666c4b7f5d1e51c85481
Fidel Nadai <fidelnadai1@outlook.es>
2023-10-05 17:56:28 -0300

    3raentrega

--- listadecompras.js
+++ listadecompras.js
@@ -44,73 +44,91 @@ const productos = [
     { name: "Skittlebrau", price: 7.50 }
   ];
 
-// Variables para mantener el estado del carrito
-const cart = [];
-let total = 0;
-
-// Función para agregar un producto al carrito
-function addToCart(productName, price) {
-    cart.push({ name: productName, price: price });
-    total += price;
-}
-
-// Función para actualizar el contenido del carrito
-function updateCart() {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  let total = 0;
+  
+  // Función para agregar un producto al carrito
+  function addToCart(productName, price) {
+      cart.push({ name: productName, price: price });
+      total += price;
+      saveCartToLocalStorage();
+  }
+  
+  // Función para actualizar el contenido del carrito
+  function updateCart() {
+      const cartItemsElement = document.getElementById("cart-items");
+      cartItemsElement.innerHTML = "";
+  
+      cart.forEach((item, index) => {
+          const listItem = document.createElement("li");
+          listItem.textContent = `${item.name} - $${item.price.toFixed(2)}`;
+          cartItemsElement.appendChild(listItem);
+      });
+  
+      const cartTotalElement = document.getElementById("cart-total");
+      cartTotalElement.textContent = `$${total.toFixed(2)}`;
+  }
+  
+  // Event listeners para los botones "Añadir al carrito"
+  const addToCartButtons = document.querySelectorAll(".add-to-cart-btn");
+  addToCartButtons.forEach((button, index) => {
+      button.addEventListener("click", () => {
+          const productDescription = document.querySelectorAll(".image-description")[index].textContent;
+          const productPrice = parseFloat(productDescription.match(/\d+\.\d+/)[0]); // Extrae el precio del texto
+  
+          addToCart(productDescription, productPrice);
+          updateCart();
+      });
+  });
+  
+  // Función para eliminar un producto del carrito
+  function removeFromCart(index) {
+    const removedItem = cart.splice(index, 1)[0];
+    total -= removedItem.price;
+  }
+  
+  // Función para actualizar el contenido del carrito
+  function updateCart() {
     const cartItemsElement = document.getElementById("cart-items");
     cartItemsElement.innerHTML = "";
-
+  
     cart.forEach((item, index) => {
         const listItem = document.createElement("li");
         listItem.textContent = `${item.name} - $${item.price.toFixed(2)}`;
+        
+        const removeButton = document.createElement("button");
+        removeButton.textContent = "Eliminar";
+        removeButton.addEventListener("click", () => {
+            removeFromCart(index);
+            updateCart();
+        });
+  
+        listItem.appendChild(removeButton);
         cartItemsElement.appendChild(listItem);
     });
-
+  
     const cartTotalElement = document.getElementById("cart-total");
     cartTotalElement.textContent = `$${total.toFixed(2)}`;
-}
-
-// Event listeners para los botones "Añadir al carrito"
-const addToCartButtons = document.querySelectorAll(".add-to-cart-btn");
-addToCartButtons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-        const productDescription = document.querySelectorAll(".image-description")[index].textContent;
-        const productPrice = parseFloat(productDescription.match(/\d+\.\d+/)[0]); // Extrae el precio del texto
-
-        addToCart(productDescription, productPrice);
+  
+  }
+  
+  // Función para guardar el carrito en el localStorage
+  function saveCartToLocalStorage() {
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+    
+    // Función para cargar el carrito desde el localStorage al cargar la página
+    function loadCartFromLocalStorage() {
+      if (localStorage.getItem("cart")) {
+        cart.length = 0; // Limpiamos el carrito actual
+        const cartData = JSON.parse(localStorage.getItem("cart"));
+        cart.push(...cartData);
         updateCart();
-    });
-});
-
-// Función para eliminar un producto del carrito
-function removeFromCart(index) {
-  const removedItem = cart.splice(index, 1)[0];
-  total -= removedItem.price;
-}
-
-// Función para actualizar el contenido del carrito
-function updateCart() {
-  const cartItemsElement = document.getElementById("cart-items");
-  cartItemsElement.innerHTML = "";
-
-  cart.forEach((item, index) => {
-      const listItem = document.createElement("li");
-      listItem.textContent = `${item.name} - $${item.price.toFixed(2)}`;
-      
-      const removeButton = document.createElement("button");
-      removeButton.textContent = "Eliminar";
-      removeButton.addEventListener("click", () => {
-          removeFromCart(index);
-          updateCart();
-      });
-
-      listItem.appendChild(removeButton);
-      cartItemsElement.appendChild(listItem);
-  });
-
-  const cartTotalElement = document.getElementById("cart-total");
-  cartTotalElement.textContent = `$${total.toFixed(2)}`;
-
-}
+      }
+    }
+    
+    // Cargar el carrito al cargar la página
+    loadCartFromLocalStorage();
 
 // Botón para comprar items
 const buyButton = document.getElementById("buy-button");
